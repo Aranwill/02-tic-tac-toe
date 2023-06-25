cbe0029fdde36ade522bbc3f54d5903eb42fbe4f
Hector Rodriguez <hectorr4747@gmail.com>
2023-06-25 13:59:29 -0300

    code format

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,23 +1,20 @@
-import { Square } from "./Square.jsx"
+import { Square } from "./Square.jsx";
 
-export function WinnerModal ({winner, resetGame}) {
-    if (winner === null) return null 
-    
-    const winnerText = winner === false ? 'Empate' : 'Gano:'
+export function WinnerModal({ winner, resetGame }) {
+  if (winner === null) return null;
+
+  const winnerText = winner === false ? "Empate" : "Gano:";
   return (
-        <section className="winner">
-          <div className="text">
-            <h2>{winnerText}</h2>
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
 
-            <header className="win">
-              {winner && <Square>{winner}</Square>}
-            </header>
+        <header className="win">{winner && <Square>{winner}</Square>}</header>
 
-            <footer>
-              <button onClick={resetGame}>Empezar de nuevo</button>
-            </footer>
-          </div>
-        </section>
-      )
-  }
-  
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  );
+}
--- src/logic/board.js
+++ src/logic/board.js
@@ -1,21 +1,21 @@
-import { WINNER_COMBOS } from "../constants.js"
+import { WINNER_COMBOS } from "../constants.js";
 
 export const checkWinnerFrom = (boardToCheck) => {
-  // revisamos todas las combinaciones ganadoras para ver si X u O gano 
+  // revisamos todas las combinaciones ganadoras para ver si X u O gano
   for (const combo of WINNER_COMBOS) {
-    const [a, b, c] = combo 
+    const [a, b, c] = combo;
     if (
       boardToCheck[a] &&
       boardToCheck[a] === boardToCheck[b] &&
-      boardToCheck[a] === boardToCheck[c] 
+      boardToCheck[a] === boardToCheck[c]
     ) {
-      return boardToCheck[a]
+      return boardToCheck[a];
     }
   }
   // Si no hay ganador
-  return null
-}
+  return null;
+};
 
 export const checkEndGame = (newBoard) => {
-  return newBoard.every((square) => square !== null)
-}
+  return newBoard.every((square) => square !== null);
+};
